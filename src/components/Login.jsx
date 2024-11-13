597cfbc1a4656a7d8db9d279aaf317732d690266
Andreea <suntandreea@protonmail.com>
2024-11-13 13:36:58 +0200

    getting user input via state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,52 @@
+import {useState} from 'react';
+
 export default function Login() {
+
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+
+  const [enteredValues, setEnteredValues] = useState({
+    mail: '',
+    pass: ''
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(enteredValues);
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  function handleValuesChange(field, event) {
+    setEnteredValues(prev => ({...prev, [field]: event.target.value}));
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          {/*<input id="email" type="email" name="email" value={enteredEmail} onChange={handleEmailChange} />          */}
+          <input id="email" type="email" name="email" value={enteredValues.mail}
+                 onChange={(e) => handleValuesChange("mail", e)} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          {/*<input id="password" type="password" name="password" value={enteredPassword}*/}
+          {/*       onChange={(e) => setEnteredPassword(e.target.value)} />*/}
+          <input id="password" type="password" name="password" value={enteredValues.pass}
+                 onChange={(e) => handleValuesChange("pass", e)} />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" type="submit">Login</button>
       </p>
     </form>
   );
